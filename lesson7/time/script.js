6eec52a891dec1ef578a272278e2c9e4f24ecc3f
Maxim <masaraksh2629@gmail.com>
2018-08-22 21:12:56 +1000

    Восьмой коммит

--- lesson7/time/script.js
+++ lesson7/time/script.js
@@ -1,9 +1,29 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
-	window.setInterval(() => {
-		let time = new Date(),
-			clock = document.getElementById("clock");
+	let clock = document.getElementById('clock');
 
-		clock.innerHTML = time.toLocaleTimeString();
-	}, 1000);
+	function timeFun() {
+		let time = new Date(),
+			h = time.getHours().toString(),
+			m = time.getMinutes().toString(),
+			s = time.getSeconds().toString();
+
+			if (h.length < 2) {
+				h = '0' + h;
+			}
+
+			if (m.length < 2) {
+				m = '0' + m;
+			}
+
+			if (s.length < 2) {
+				s = '0' + s;
+			}
+
+		let timeString = (`${h}:${m}:${s}`);
+
+			clock.textContent = timeString;
+	}
+
+	setInterval(timeFun, 1000);
 });
